45edc9b3a3e5d2344c700d96c8f8e4d7c1553280
dkuzmin <dmitrii.kuzmin@apstecsystems.com>
2021-10-12 12:11:33 +0300

    dateTimeFormatter rename (CONSTANT_STYLE)

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -5,13 +5,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
     }
 
     public static String formatLocalDateTime(LocalDateTime localDateTime) {
-        return localDateTime.format(dateTimeFormatter);
+        return localDateTime.format(DATE_TIME_FORMATTER);
     }
 }
