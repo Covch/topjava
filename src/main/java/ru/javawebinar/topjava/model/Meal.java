8af981c2d3eeddbef87b36b251ec121544235d85
dkuzmin <dmitrii.kuzmin@apstecsystems.com>
2021-10-19 21:44:14 +0300

    delete unused userId field

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,8 +6,6 @@ import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
 
-    private final Integer userId;
-
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -15,23 +13,14 @@ public class Meal extends AbstractBaseEntity {
     private final int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, null, dateTime, description, calories);
+        this(null, dateTime, description, calories);
     }
 
     public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this(id, null, dateTime, description, calories);
-    }
-
-    public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
         super(id);
-        this.userId = userId;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-    }
-
-    public Integer getUserId() {
-        return userId;
     }
 
     public LocalDateTime getDateTime() {
@@ -58,7 +47,6 @@ public class Meal extends AbstractBaseEntity {
     public String toString() {
         return "Meal{" +
                 "id=" + id +
-                ", userId=" + userId +
                 ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
